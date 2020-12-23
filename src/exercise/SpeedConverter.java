2a5f5e7e9c73b556df4ea9a7e275b34968f4fa49
Sri Prasanth R <rsriprasanth@gmail.com>
2020-12-23 17:46:05 +0530

    SpeedConverter Exercise

---
+++ src/exercise/SpeedConverter.java
@@ -0,0 +1,24 @@
+package exercise;
+
+public class SpeedConverter {
+	 public static long toMilesPerHour(double kilometersPerHour){
+
+	        if(kilometersPerHour<0) {
+	            return -1;
+	        }
+	        double milesPerhour = kilometersPerHour/(1.609);
+	        return(Math.round(milesPerhour));
+	    }
+
+	    public static void printConversion(double kilometersPerHour){
+	        if(kilometersPerHour < 0){
+	            System.out.println("Invalid Value");
+	        } else{
+	            long milesPerHour = toMilesPerHour(kilometersPerHour);
+	            System.out.println(kilometersPerHour + " km/h = " + milesPerHour + " mi/h");
+	        }
+	    }
+	    public static void main(String[] args) {
+	        printConversion(50);
+	    }
+}
